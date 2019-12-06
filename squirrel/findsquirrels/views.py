1fcac969d4194decbd491e649bfe3cb95d6f2156
cicyhqy <qh2218@columbia.edu>
2019-12-05 23:41:28 -0500

    functions mark more points

--- squirrel/findsquirrels/views.py
+++ squirrel/findsquirrels/views.py
@@ -6,4 +6,14 @@ def map(request):
 	return render(request,'findsquirrels/map.html')
 
 
+def getlocation(request):
+	squirrels = Squirrel.objects.values_list("unique_squirrel_id","y","x")
+	sample=squirrels.sample(1)
+	context = {
+		'id': sample.unqunique_squirrel_id
+		'y':sample.y
+		'x':sample.x 
+	}
+	return render(request,'findsquirrels/map.html',context)
+	
     
