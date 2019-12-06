4ac176814ca243fff0a7bc950fb1d3dc4ceeaace
Xuan Chen <thu_chenxuan@126.com>
2019-12-06 03:06:50 +0000

    add form

--- squirrel/findsquirrels/views.py
+++ squirrel/findsquirrels/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Squirrel
+from .forms import SquirrelForm
 
 def map(request):
 	return render(request,'map.html')
 
+def all_squirrels(request):
+    squirrels = Squirrel.objects.all()
+    context = {
+            'squirrels':squirrels,
+    }
+    return render(request,'findsquirrels/sightings.html',context)
 
     
