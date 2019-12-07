099144ffcad63b0e796bcbe3e8740b8dcb1a7876
Xuan Chen <thu_chenxuan@126.com>
2019-12-07 23:52:39 +0000

    delete comments

--- squirrel/findsquirrels/views.py
+++ squirrel/findsquirrels/views.py
@@ -44,34 +44,14 @@ def edit_squirrel(request,unique_squirrel_id):
         if form.is_valid():
             form.save()
             return HttpResponse(f'Updated squirrel {unique_squirrel_id}')
-#    elif request.method == 'DELETE':
-#       form = SquirrelForm(request.DELETE,instance=squirrel)
-#       if form.isvalid():
-#           form.delete()
-#           return HttpResponse(f"Deleted squirrel {unique_squirrel_id}")
     else:
         form = SquirrelForm(instance=squirrel)
-        #build a new empty form
 
     context = {
             'form':form,
     }
 
     return render(request,'sightings/add.html',context)
-
-#def delete_squirrel(request, unique_squirrel_id):
-#   squirrel = Squirrel.objects.get(unique_squirrel_id = unique_squirrel_id)
-#   if request.method == 'POST':
-#       form = SquirrelForm(request.POST,instance=squirrel)
-#       if form.isvalid():
-#           form.delete()
-#           return HttpResponse(f"Deleted squirrel {unique_squirrel_id}")
-#   else:
-#       form = SquirrelForm(instance=squirrel)
-#   context = {
-#           'form':form,
-#   }
-#   return render(request,'sightings/add.html',context)
 
 
 def stats(request):
