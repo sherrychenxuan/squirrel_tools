03c65563dd9c6df37850b269fdec77eb5ae7a784
Xuan Chen <thu_chenxuan@126.com>
2019-12-08 01:49:28 +0000

    finish stats with qianyu

--- squirrel/findsquirrels/urls.py
+++ squirrel/findsquirrels/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
 	path('map/',views.select_samples),
         path('sightings/',views.all_squirrels),
+        path('sightings/stats',views.stats),
         path('sightings/add',views.add_squirrel),
         path('sightings/<str:unique_squirrel_id>',views.edit_squirrel),
-	path('sightings/stats',views.stats),
 ]
 
